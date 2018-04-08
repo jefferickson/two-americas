5d7770efc1f3bef42fce286a447806356cad751a
Jeff Erickson <jeff@erick.so>
2018-04-08 14:46:47 -0400

    default csv export settings

--- extract/tocsv.go
+++ extract/tocsv.go
@@ -14,15 +14,19 @@ func main() {
 func extractTweets() {
 	// Set filters, selectors (can be empty for all tweets)
 	filter := bson.M{}
-	selector := bson.M{"countylisting": 1}
+	selector := bson.M{"status.fulltext": 1,
+		"tweetid": 1,
+		"counter": 1}
 
 	// Fetch all tweets
 	allTweets := model.AllTweets(filter, selector)
 
 	// Loop over tweets and write out
 	for _, tweet := range allTweets {
-		fmt.Println(tweet.CountyListing.Lon,
-			tweet.CountyListing.Lat,
-			tweet.CountyListing.State)
+		fmt.Println(tweet.TweetID, "\t",
+			tweet.Counter.GeoID, "\t",
+			tweet.Counter.Topic, "\t",
+			tweet.Status.FullText,
+		)
 	}
 }
